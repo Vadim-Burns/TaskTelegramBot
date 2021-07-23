e666bf079b3498d8d0fb1266655e102f07cfef7e
Vadim Tsindyaykin <vadim.burns@protonmail.com>
2021-07-23 04:03:36 +0300

    first greeting version
    Signed-off-by: Vadim Tsindyaykin <vadim.burns@protonmail.com>

--- bot/bot.py
+++ bot/bot.py
@@ -2,3 +2,31 @@
 This file includes all business logic of bot.
 All handlers are only here.
 """
+
+import telebot
+import config
+import database as db
+
+bot = telebot.TeleBot(config.bot_token, parse_mode=None)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Howdy! What's your name, dude?")
+    bot.register_next_step_handler(message, callback=saving_name_handler)
+
+
+def saving_name_handler(message):
+    bot.send_message(
+        message.chat.id,
+        "Okay, {name} is enough good name for me.\nNice to meet you, {name}!".format(name=message.text)
+    )
+
+
+@bot.message_handler(content_types=['text'])
+def wrong_command_handler(message):
+    bot.reply_to(message, "Sorry, but i don't understand you, dude")
+
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
--- bot/config.py
+++ bot/config.py
@@ -1,3 +1,8 @@
 """
 This file contains configuration variables
 """
+
+import os
+
+# Telegram bot token
+bot_token = os.environ['BOT_TOKEN']
